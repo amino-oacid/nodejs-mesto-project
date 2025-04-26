13852a94d8d320f04eca1d2acf3cc42537662af4
Amina Rakhbari <amina.rakhbari@yandex.ru>
2025-04-26 21:46:01 +0300

    Поправила ошибку по умолчанию

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomError, IError } from '../custom-error';
+import {
+  CustomError,
+  errorMessages,
+  IError,
+  statusCodes,
+} from '../custom-error';
 
 const errorsMiddleware = (error: IError, req: Request, res: Response, next: NextFunction) => {
-  let statusCode = error.statusCode || 400;
-  let message = error.message || 'Ошибка на сервере';
+  let statusCode = error.statusCode || statusCodes.internalServerError;
+  let message = error.message || errorMessages.internalServerError;
 
   if (error instanceof CustomError) {
     statusCode = error.statusCode;
